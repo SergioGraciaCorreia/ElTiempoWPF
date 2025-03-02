fae23271dbc0dc7c62555ce995568c98d17a2aab
Keko <sergiograciacorreia@gmail.com>
2025-03-02 17:10:46 +0100

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ElTiempoWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
